1b369507152d8027542bc11b9c1b00e75bb83d36
CE Libertador Bolívar <64515162+celibertadorbolivar@users.noreply.github.com>
2024-08-25 21:40:18 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -392,7 +392,7 @@ function inicial() {
     if (opcionSeleccionada === "" || textoInput === "") {
       alert("Por favor selecciona una opción y escribe un indicador.");
     } else {
-      var enlaceGenerado = "https://celibertadorbolivar.github.io/web/boletas/inicial/" + opcionSeleccionada + "/" + textoInput + ".pdf";
+      var enlaceGenerado = "https://evaipcelb.github.io/celb/boletas/inicial/" + opcionSeleccionada + "/" + textoInput + ".pdf";
       var resultadoElement = document.getElementById("resultado1");
     document.getElementById("resultado1").innerHTML= enlaceGenerado;
     window.location=document.getElementById("resultado1").innerHTML;
