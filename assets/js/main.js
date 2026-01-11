22a1a924e34d0b87775d4fed942514ff414dfe5f
UE Libertador Bolívar <64515162+uelibertadorbolivar@users.noreply.github.com>
2026-01-11 16:30:22 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -422,7 +422,7 @@ function media() {
   if (opcionSeleccionada === "" || textoInput === "") {
     alert("Por favor selecciona una opción y escribe un indicador.");
   } else {
-    var enlaceGenerado = "https://mediagenerale.github.io/celb/boletas/media/" + opcionSeleccionada + "/" + textoInput + ".pdf";
+    var enlaceGenerado = "https://mediagenerale.github.io/celb/media/" + opcionSeleccionada + "/" + textoInput + ".pdf";
     var resultadoElement = document.getElementById("resultado1");
   document.getElementById("resultado1").innerHTML= enlaceGenerado;
   window.location=document.getElementById("resultado1").innerHTML;
@@ -479,4 +479,5 @@ function mediac() {
  window.location=document.getElementById("resultado2").innerHTML;
 }
 }
+
 
